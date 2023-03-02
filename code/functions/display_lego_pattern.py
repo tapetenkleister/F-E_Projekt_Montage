efab24f4a489e33ec20e3c7f73d08467341ef75b
Stephan <klst1026@h-ka.de>
2023-03-02 14:49:09 +0100

    new matrix fix and other stuff

--- code/functions/display_lego_pattern.py
+++ code/functions/display_lego_pattern.py
@@ -11,17 +11,23 @@ def display_lego_pattern(matrix:np.ndarray)->None:
 
     Args:
         matrix (np.ndarray): Array to be displayed
-    """    
+    """ 
+
+    # Get the length of the first row
+    first_row_length = len(matrix[0])
     
+    # Use slicing to check if all other rows have the same length
+    if np.all([len(row) != first_row_length for row in matrix[1:]]):
+        print('error')
+        raise ValueError("Input matrix doesn't consist of rows with same length")
+    
+
     # color_map = {0: [0, 200, 0], 1: [255, 255, 0],
     #             2: [0, 0, 255], 3: [255, 0, 0],4: [0, 90, 0]}
     color_map = {'green': [0, 200, 0], 'yellow': [255, 255, 0],'white': [255, 255, 255],
-                 'blue': [0, 0, 255], 'red': [255, 0, 0],'black': [0, 0, 0]}
+                 'blue': [0, 0, 255], 'red': [255, 0, 0],'black': [0, 0, 0],'lime':[0,150,0]}
 
 
     # Convert the color matrix to a 3D array of RGB values
     rgb_colors = np.array([[color_map[c] for c in row] for row in matrix])
-
-    # Display the image
-    plt.imshow(rgb_colors)
-    plt.show()
+    return rgb_colors
