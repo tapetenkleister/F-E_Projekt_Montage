5e316aa66d667ed2d612a87c1740cbb9191f7cce
Q-Pegasus <sophia@reiter-ka.de>
2023-03-11 15:14:40 +0100

    rename Thumbnail and updated gui

--- assembly_step_detection/gui_v4 _layout.py
+++ assembly_step_detection/gui_v4 _layout.py
@@ -2,6 +2,8 @@ import os
 import cv2
 import sys
 import shutil
+import time
+import subprocess
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -9,7 +11,6 @@ from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import *
 
 from functions import *
-
 
 class Fenster(QMainWindow):
 
@@ -19,8 +20,8 @@ class Fenster(QMainWindow):
         
     def initMe(self):
         self.p = None
-        self.text = QLabel(' ', self)
-        self.text.setGeometry(0,1600, 1000, 50)
+        self.result = QLabel(' ', self)
+        self.result.setGeometry(0,1600, 1000, 50)      
         hight = 700
         width = 700
     
@@ -58,20 +59,17 @@ class Fenster(QMainWindow):
             self.construction_plan.addItem(folder)
         self.construction_plan.currentIndexChanged.connect(self.select_construct_plan)
 
-        # creates a button to to show the current progress step
-        button_progress = QPushButton('Show progress', self)
+        # button to to show the current progress step
+        button_progress = QPushButton('Detect assembly step', self)
         button_progress.setFixedSize(QSize(300, 100))
         button_progress.setToolTip('Shows the current <b>progress</b> for the selected plan')
         button_progress.clicked.connect(self.detect_progress)
 
-        # creates a button to capture an image
+        # button to capture an image
         button_photo = QPushButton('Take photo', self)
         button_photo.setFixedSize(QSize(300, 100))
         button_photo.setToolTip('take <b>Photo</b>')
         button_photo.clicked.connect(self.start_process)
-        # self.text2 = QPlainTextEdit()
-        # self.text2.setReadOnly(True)
-        # self.text2.setGeometry(1700, 500, 200, 200)
 
         # button to save new construction template
         button_save = QPushButton('Save new template', self)
@@ -94,26 +92,21 @@ class Fenster(QMainWindow):
         instruction.setShortcut('Ctrl+I')
         instruction.triggered.connect(self.show_instructions)
 
-        # # creating action for changing save folder
-        # change_folder = QAction('change &saving location',self)
-        # change_folder.triggered.connect(self.change_folder)
-        # change_folder.setShortcut('Ctrl+S')
-
         # creates a menu bar
         menuBar = self.menuBar()
         menu = menuBar.addMenu('&Menu')
  
         menu.addAction(capture_picture)
-        #menu.addAction(show_progress)
-        #menu.addAction(change_folder)
         menu.addAction(instruction)
         menu.addAction(exitMe)
 
         self.statusBar().showMessage('Assembly inspection from Stephan Klotz, Jennifer Sissi Lange, Sophia Reiter')
+
+        # Create a widget for the window's main layout
         widget = QWidget()
         # Create an outer layout
         outerLayout = QHBoxLayout(widget)
-        # Create a layout to show the progress
+        # Create a layout to show the assembly step
         leftLayout = QVBoxLayout(widget)
         # Create a layout for the upper images
         topLeftLayout = QHBoxLayout()
@@ -123,81 +116,82 @@ class Fenster(QMainWindow):
         rightLayout = QVBoxLayout(widget)
         # Set the alignment of the right layout
         rightLayout.setAlignment(Qt.AlignHCenter)
-
-        # Add labels the leftLayout
+         # Add labels the leftLayout
         topLeftLayout.addWidget(self.label_tl)
         topLeftLayout.addWidget(self.label_tr)
         lowLeftLayout.addWidget(self.label_bl)
         lowLeftLayout.addWidget(self.label_br)
         leftLayout.addLayout(topLeftLayout)
         leftLayout.addLayout(lowLeftLayout)
-        # add buttopns, combobox and label to the rightLayout
+        # Add buttons, combobox and label to the rightLayout
         rightLayout.addWidget(self.label_plan)
         rightLayout.addWidget(self.construction_plan)
         rightLayout.addWidget(button_photo)
         rightLayout.addWidget(button_progress)
         rightLayout.addWidget(button_save)
-        rightLayout.addWidget(self.text)
-       
+        rightLayout.addWidget(self.result)
+        # Add the layouts to the outer layout
         outerLayout.addLayout(leftLayout)
         outerLayout.addLayout(rightLayout)
         self.setCentralWidget(widget)
-
         # Set the window's main layout
         self.setLayout(outerLayout)
         self.setGeometry(100, 100, 2750, 2000)
         self.setWindowTitle('Assembly inspection')
         self.show()
-      
 
-    # def change_folder(self):
-    #     # open the dialog to select path
-    #     path = QFileDialog.getExistingDirectory(self, 
-    #                                             "Picture Location", "")
-    #     # if path is selected
-    #     if path:
-    #         # update the path
-    #         self.folder = path
 
     def select_construct_plan(self, i):
-        # set thumbnail.png matching the construction_plan.currentText() as pixmap
+        """ This function is called when the user selects a construction plan from the combobox.
+        It shows the corresponding image (thumbnail) of the selected plan.
+        Args:
+            i: index of the selected item of the combobox
+        """     
         for folder in os.listdir('Templates'):
-            print('1' + folder)
-            print('2' + self.construction_plan.currentText())
             if self.construction_plan.currentText() == folder:
                 pixmap_plan = QPixmap('Templates/' + self.construction_plan.currentText() + '/' + self.construction_plan.currentText() + '_thumbnail.png')
-                #self.label_plan.setPixmap(pixmap_plan)
                 self.label_plan.setPixmap(pixmap_plan.scaled(700,700,Qt.KeepAspectRatio))
-        #print(self.construction_plan.currentText())
 
-    #def message(self, s):
-    #    self.text2.appendPlainText(s)
 
     @pyqtSlot()    
     def start_process(self):
-        if self.p is None:  # No process running.
-            self.message("Executing process")
-            self.p = QProcess()  # Keep a reference to the QProcess (e.g. on self) while it's running.
-            self.p.finished.connect(self.process_finished)  # Clean up once complete.
-            self.p.start("python3", ['take_image_btn.py']) #selects file to be executed
-        self.pixmap_tl = QPixmap('Images_Results/image.jpg')
-        self.label_tl.setPixmap(self.pixmap_tl.scaled(1500, 1500, Qt.KeepAspectRatio))
-        self.label_tl.setGeometry(50, 100, 1500, 1500)
-        self.label_bl.clear()
-        self.label_tr.clear()
-        self.label_br.clear()
+        """ This function is called when the user clicks on the button to capture an image.
+        It starts the process to capture an image and shows the image in the corresponding label.
+        """     
+        # if no process is running
+        if self.p is None:  
+            # self.message("Executing process")
+            # Keep a reference to the QProcess (e.g. on self) while it's running.
+            self.p = QProcess()  
+            # Clean up once complete
+            self.p.finished.connect(self.process_finished)  
+            #selects file to be executed
+            self.p.start("python3", ['take_image_btn.py']) 
+            # waits until file is executed
+            self.p.waitForFinished()
+            # shows the results
+            self.pixmap_tl = QPixmap('Images_Results/image.jpg')
+            self.label_tl.setPixmap(self.pixmap_tl.scaled(1500, 1500, Qt.KeepAspectRatio))
+            self.label_tl.setGeometry(50, 100, 1500, 1500)
+            self.label_bl.clear()
+            self.label_tr.clear()
+            self.label_br.clear()
 
         
     def process_finished(self):
-        #self.message("Process finished.")
+        """ This function is called when the process to capture an image or to detect assembly step is finished.
+        """
         self.p = None
 
 
     def save_new_plan(self):
-        # user should be able to enter the name of the new template and the amount of nubs on the longest side
+        """ This function creates a new folder in the Templates folder and copies all images from the selected folder into the new folder.
+        It calls the function to save a new matrix. 
+        """       
+        # input dialog to enter the name of a new template 
         text, ok = QInputDialog.getText(self, 'save new plan', 'Please enter template name (with a capital letter):') # entering the file name
         self.template_name = text
-        
+        # input dialog to enter the amount of nubs on the longest side
         if ok:
             number, ok = QInputDialog.getInt(self, 'save new plan', 'Please enter the amount of nubs on the longest side: ') # entering the file name
             self.longest_side = number
@@ -206,7 +200,6 @@ class Fenster(QMainWindow):
                 newpath = 'Templates/' + self.template_name
                 if not os.path.exists(newpath):
                     os.makedirs(newpath)
-
                 # open the dialog to select path
                 path = QFileDialog.getExistingDirectory(self, 
                                                         "Picture Location", "")
@@ -214,24 +207,25 @@ class Fenster(QMainWindow):
                 if path:
                     # update the path
                     self.folder = path
-
                 #select folder and copy images in selected folder
                 for file in os.listdir(self.folder):
                     if file.endswith(".png"):
                         shutil.copy(self.folder +"/"+ file, newpath)
-                # crreate json file for the new template
+                # create json file for the new template
                 new_matrix = safe_new_matrix(self.template_name, self.longest_side)
-            
         else:
             # show gui again
             self.show()
                 
 
     def show_instructions(self):
-        # show instructions how to use the application
+        """ This function shows the instructions how to use the application.
+        """   
+        # create a message box     
         info = QMessageBox()
         info.setWindowTitle("Instruction")
         instruction = open('Docs/instructions.txt','r')
+        # read the instructions from the file and show them in the message box
         info.setText("This is the main text!" + "\n" + instruction.read())
         instruction.close()
         x = info.exec_()
@@ -239,22 +233,33 @@ class Fenster(QMainWindow):
 
     @pyqtSlot()    
     def detect_progress(self):
+        """ This function is called when the user clicks on the button to detect the progress.
+        It starts the process to detect the progress and shows the result in the corresponding label.
+        """
         if self.p is None:  # No process running.
             #self.message("Executing process")
             self.p = QProcess()  # Keep a reference to the QProcess (e.g. on self) while it's running.
             self.p.finished.connect(self.process_finished)  # Clean up once complete.
             self.p.start("python3", ['detect_btn.py']) #selects file to be executed
-        with open ('Images_Results/result.txt', 'r') as f:
-            line = f.read()
-        self.text.setText(line)
-        self.pixmap_tr = QPixmap('Images_Results/circles.jpg')
-        self.label_tr.setPixmap(self.pixmap_tr.scaled(700, 700, Qt.KeepAspectRatio))
-        self.pixmap_tl = QPixmap('Images_Results/image.jpg')
-        self.label_tl.setPixmap(self.pixmap_tl.scaled(700, 700, Qt.KeepAspectRatio))
-        self.pixmap_br = QPixmap('Images_Results/result.jpg')
-        self.label_br.setPixmap(self.pixmap_br.scaled(700, 700, Qt.KeepAspectRatio))
-        self.pixmap_bl = QPixmap('Images_Results/color_matrix.png')
-        self.label_bl.setPixmap(self.pixmap_bl.scaled(700, 700, Qt.KeepAspectRatio))
+            # make sure detect_btn.py is executed before the following code is executed
+            self.p.waitForFinished()
+            # read the result.txt file and show the result in the text box
+            with open ('Images_Results/result.txt', 'r') as f:
+                line = f.read()
+            self.result.setText(line)
+            self.pixmap_tr = QPixmap('Images_Results/circles.jpg')
+            self.label_tr.setPixmap(self.pixmap_tr.scaled(700, 700, Qt.KeepAspectRatio))
+            self.pixmap_tl = QPixmap('Images_Results/image.jpg')
+            self.label_tl.setPixmap(self.pixmap_tl.scaled(700, 700, Qt.KeepAspectRatio))
+            self.pixmap_br = QPixmap('Images_Results/result.jpg')
+            self.label_br.setPixmap(self.pixmap_br.scaled(700, 700, Qt.KeepAspectRatio))
+            self.pixmap_bl = QPixmap('Images_Results/color_matrix.png')
+            self.label_bl.setPixmap(self.pixmap_bl.scaled(700, 700, Qt.KeepAspectRatio))
+            from detect_btn import detected_assembly_step
+            detected_template = detected_assembly_step.split(' ')
+            # change index of construction_plan to the detected template[0]
+            self.construction_plan.setCurrentIndex(self.construction_plan.findText(detected_template[0]))
+
 
  
 if __name__ == '__main__':        
